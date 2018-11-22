dd66eef7c11e3e90d572a0d5ac223391da7b6e50
jaclynflomen <j_floman@fanshaweonline.ca>
2018-11-22 17:36:44 -0500

    contact function

--- includes/sendEmail.php
+++ includes/sendEmail.php
@@ -1,4 +1,16 @@
 <?php 
 
-echo 'Submission sent!';
+var_dump($_GET);
+function send_email (){
+    $to = 'j_floman@fanshaweonline.ca';
+    $subject = 'This is the subject if the email';
+    $message = 'Email message body';
+
+    fake_mail($to, $subject, $message);
+    //In your server, use the following line instead
+    //mail($to, $subject, $message);
+}
+
+send_email();
+
 ?>
