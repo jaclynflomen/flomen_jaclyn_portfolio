6cf1313901713ba5596ed5656d6d40dc10bf8e0e
jaclynflomen <j_floman@fanshaweonline.ca>
2018-11-23 15:10:20 -0500

    more changes to layout

--- includes/sendEmail.php
+++ includes/sendEmail.php
@@ -1,12 +1,25 @@
 <?php 
 
-var_dump($_GET);
-function send_email (){
-    $to = 'j_floman@fanshaweonline.ca';
-    $subject = 'This is the subject if the email';
-    $message = 'Email message body';
+// // var_dump($_GET);
+// function mail($to, $subject, $message, $headers) {
+//     echo '=== Fake Emails ==='.PHP_EOL;
+//     echo 'Subject: '.$subject.PHP_EOL;
+//     echo 'Email to:'.$to.PHP_EOL;
+//     echo 'Message: '.$message.PHP_EOL;
+//     echo '==== Emails End ===='.PHP_EOL;
 
-    fake_mail($to, $subject, $message);
+//     return true;
+// }
+
+//var_dump($_GET);
+function send_email (){
+    $to = 'jaclynflomen@gmail.com';
+    $subject = 'This is an email from '.$_GET['name'];
+    $message = $_GET['comments'];
+    $headers = 'From: noreply@YOURDOMAIN.com';
+    $headers .= 'Reply-To: '.$_GET['email'];
+
+    mail($to, $subject, $message, $headers);
     //In your server, use the following line instead
     //mail($to, $subject, $message);
 }
