4d5c8b38ca038d20f3c645e458962d2e5779065e
jaclynflomen <j_floman@fanshaweonline.ca>
2018-11-30 21:15:10 -0500

    lightbox working

--- js/main.js
+++ js/main.js
@@ -1,94 +1,27 @@
-// // Open the Modal
-// function openModal() {
-//     document.getElementById('myModal').style.display = "block";
-//   }
-  
-//   // Close the Modal
-//   function closeModal() {
-//     document.getElementById('myModal').style.display = "none";
-//   }
-  
-//   var slideIndex = 1;
-//   showSlides(slideIndex);
-  
-//   // Next/previous controls
-//   function plusSlides(n) {
-//     showSlides(slideIndex += n);
-//   }
-  
-//   // Thumbnail image controls
-//   function currentSlide(n) {
-//     showSlides(slideIndex = n);
-//   }
-  
-//   function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     var dots = document.getElementsByClassName("demo");
-//     var captionText = document.getElementById("caption");
-//     if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex-1].style.display = "block";
-//     dots[slideIndex-1].className += " active";
-//     captionText.innerHTML = dots[slideIndex-1].alt;
-//   }
 
+halkaBox.run("galleryDemo");
 
-var openPhotoSwipe = function() {
-  var pswpElement = document.querySelectorAll('.pswp')[0];
+halkaBox.options({
 
-  //build items array now
+  // hide control buttons on mobile devices
+  hideButtons: true, 
 
-  var items = [
-    {
-      src: '/images/Work/SITL.png',
-      w:600,
-      h: 515
-    }, {
-      src: '/images/Work/newsblast_custom_ad_2.jpg',
-      w: 1275,
-      h: 825
-    }, {
-      src: '/images/Work/pride_day.jpg',
-      w: 1080,
-      h: 1115
-    }, {
-      src: '/images/Work/thanksgiving.jpg',
-      w: 490,
-      h: 565
-    }, {
-      src: '/images/Work/waffle_day.jpg',
-      w: 455,
-      h:455
-    }
-  ];
+  // "slide" or "fade"
+  animation: "fade",
 
-  var options = {
-    history:false,
-    focus:false,
+  // "dark" or "light"
+  theme: "dark", 
 
-    showAnimationDuration: 0,
-    hideAnimationDuration:0,
+  // the number of images to preload
+  preload: 2,
 
-    index: 0
+  // swipe down to close
+  swipeDownToClose: false,
 
-  };
+  // switch between images with mousewheel
+  nextPrevOnWheel: true,
 
-  var gallery = new
-  PhotoSwipe(pswpElement, PhotoswipeUI_Default, items, options);
-
-  gallery.init();
-
-openPhotoSwipe();
-
-document.getElementById('btn').onclick = openPhotoSwipe;
-
-
-
-}
+  // is zoomable
+  isZoomable: true
+       
+});
