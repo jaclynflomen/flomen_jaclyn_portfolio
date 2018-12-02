8cbe684317b12274088e64d4c851b505e196fa0d
jaclynflomen <j_floman@fanshaweonline.ca>
2018-12-02 17:02:00 -0500

    adding other php files

--- includes/connect.php
+++ includes/connect.php
@@ -27,4 +27,21 @@ if (isset($_GET["ID"])) {
 
     echo json_encode($rows);
 }
+
+//get one item from database
+if (isset($_GET["art_id"])) {
+    $aboutme = $_GET["art_id"];
+
+    $myQuery = "SELECT * FROM tbl_portfoliowork WHERE art_id='$art'";
+
+    $result = mysqli_query($conn, $myQuery);
+    $rows = array();
+
+    //fill the array with the result set and send it to the browser
+    while($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+
+    echo json_encode($rows);
+}
 ?>
---
+++ includes/function.php
@@ -0,0 +1,32 @@
+<?php
+    include 'connect.php';
+    // get one art first
+    function get_single_art($pdo, $art) {
+        $query = "SELECT * FROM tbl_portfoliowork WHERE art_id = $art"; 
+        //attach the id in the thumnail to ensure you are getting the right image
+
+        $get_art = $pdo->query($query);
+        $results = array();
+
+        while($row = $get_art->fetch(PDO::FETCH_ASSOC)) {
+            $results[] = $row;
+        }
+        return $results;
+    }
+
+    //get all movies
+    function get_all_art($pdo) {
+        $query = "SELECT * FROM tbl_portfoliowork";
+        
+        $get_art = $pdo->query($query);
+        $results = array();
+        
+        while($row = $get_art->fetch(PDO::FETCH_ASSOC)) {
+            $results[] = $row;
+        }
+        
+        return $results; 
+        }
+
+
+?>
