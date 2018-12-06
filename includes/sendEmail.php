7408a5fab1c0361a0135e66680f53315a5c198b0
jaclynflomen <j_floman@fanshaweonline.ca>
2018-12-06 16:26:44 -0500

    working on making contact pg work live

--- includes/sendEmail.php
+++ includes/sendEmail.php
@@ -1,31 +1,27 @@
 <?php 
 
-// // var_dump($_POST);
-// function fake_mail($to, $subject, $message, $headers) {
+function send_email (){
+    //Email validations: checking required fields
+
+    if(isset($_GET['name'])
+    ||isset($_GET['email'])
+    ||isset($_GET['comments'])) {
+        echo 'You are missing some required fields';
+        exit;
+    }
+
+    $to = 'jaclyn@jaclynflomen.com';
+    $subject = 'This is an email from '.$_GET['name'];
+    $message = $_GET['comments'];
+    $headers = 'From: noreply@jaclynflomen.com';
+    $headers .= 'Reply-To: '.$_GET['email'];
+
 //     echo '=== Fake Emails ==='.PHP_EOL;
 //     echo 'Subject: '.$subject.PHP_EOL;
 //     echo 'Email to:'.$to.PHP_EOL;
 //     echo 'Message: '.$message.PHP_EOL;
 //     echo '==== Emails End ===='.PHP_EOL;
-
-//     return true;
 // }
-
-function send_email (){
-    //Email validations: checking required fields
-
-    if(empty($_POST['name'])
-        || empty($_POST['email'])
-        || empty($_POST['comments'])) {
-            echo 'You are missing some required fields';
-            exit;
-        }
-
-    $to = 'jaclynflomen@gmail.com';
-    $subject = 'This is an email from '.$_POST['name'];
-    $message = $_POST['comments'];
-    $headers = 'From: noreply@YOURDOMAIN.com';
-    $headers .= 'Reply-To: '.$_POST['email'];
 
     mail($to, $subject, $message, $headers);
     //In your server, use the following line instead
