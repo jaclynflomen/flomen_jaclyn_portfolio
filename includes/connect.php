efa6713f68901ef16db4b8f34fd00cfee1d4c17d
jaclynflomen <j_floman@fanshaweonline.ca>
2018-12-06 15:37:25 -0500

    update

--- includes/connect.php
+++ includes/connect.php
@@ -28,3 +28,22 @@ if (isset($_GET["ID"])) {
     echo json_encode($rows);
 }
 ?>
+
+<?php 
+
+$user = "root";
+$pwd = "root";
+
+try {
+    $conn = new PDO('mysql:host=localhost;dbname=db_portfolio',$user, $pwd);
+    //this is the same thing as going $host = "localhost" etc but just in one line
+    //var_dump($conn); //check connection - instead of doing a var element, use var_dump to 
+                    //show because it is an object
+    
+
+} catch (PDOException $exception) {
+    echo 'connect error!' . $exception->getMessage();
+}
+
+
+?>
