d1a65ca3c898d895d12ef676f0beea546f27965b
jaclynflomen <j_floman@fanshaweonline.ca>
2018-12-09 13:53:44 -0500

    friends are awesome

--- includes/index.php
+++ includes/index.php
@@ -3,10 +3,13 @@
     
     //single art route
     if (isset($_GET["category"])){
-        $data = get_single_art($conn, $_GET["category"]);
+        $data = get_single_art($pdo, $_GET["category"]);
+        echo json_encode($data);
+    } elseif (isset($_GET["ID"])){
+        $data = get_svg($pdo, $_GET["ID"]);
         echo json_encode($data);
     } else {
-        $data = get_all_art($conn);
+        $data = get_all_art($pdo);
         echo json_encode($data);
         //can use this for portfolio pieces too - does not only apply to media
     }
